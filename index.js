e1b6bd1674968a758788feaea323e583b896fb20
Unrealrojo234 <otienoryan812@gmail.com>
2024-08-28 14:15:16 +0300

    Minor Update 1.0

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "5mb" }));
 
@@ -23,22 +19,20 @@ app.post("/news", async (req, res) => {
   try {
     const news = await News.create(req.body);
     res.status(200).json(news);
-    
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 //Getting data from the database
-app.get('/news',async(req,res)=>{
- try{
-  const news = await News.find({});
-  res.status(200).json(news)
-
- }catch(error){
-  res.status(500).json({message: error.message})
- }
-})
+app.get("/news", async (req, res) => {
+  try {
+    const news = await News.find({});
+    res.status(200).json(news);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Kisii University Gossip Club Backend</h1>");
@@ -47,6 +41,6 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   mongoose.connect(dbConnectionString).then(() => {
     console.log("Successfully connected to the database");
-    console.log("Server is running on port 3000");
   });
+  console.log("Server is running on port 3000");
 });
